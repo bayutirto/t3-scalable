d8c9a620349d969994a1188acb43fb4cf8ab99da
Bayu Tirto <bang.bayutirto@gmail.com>
2022-11-03 13:30:00 +0700

    add: auth

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,12 +1,16 @@
 // src/server/router/index.ts
-import { createRouter } from './context';
 import superjson from 'superjson';
+import { createRouter } from './context';
 
+import { signUpRouter } from './auth';
 import { exampleRouter } from './example';
+import { userRouter } from './user';
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('example.', exampleRouter);
+  .merge('example.', exampleRouter)
+  .merge('auth.', signUpRouter)
+  .merge('user.', userRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
